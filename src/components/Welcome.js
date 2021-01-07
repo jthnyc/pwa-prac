d1003a7b8d7648ff223c0ace319d9e9ee812ce12
Joanna Huang <joannathhuang@gmail.com>
2021-01-07 18:00:11 -0500

    Updating Welcome component with image, details, and countdown

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,36 @@
 import React from "react";
 import styled, {keyframes} from "styled-components";
+import Countdown from "react-countdown";
 
 const Welcome = () => {
+  const renderer = ({hours, minutes, seconds, completed}) => {
+    if (completed) {
+      return (
+        <div>
+          <span>Time to go!</span>
+        </div>
+      );
+    } else {
+      return (
+        <span>
+          {hours}:{minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
   return (
     <Container>
       <HeroImage>
         <img src="http://placekitten.com/600/700" alt="placeholder" />
       </HeroImage>
-      <DateInfo>Nov 20, 2021</DateInfo>
+      <DateInfo>
+        <Details>
+          <h3>New York, NY</h3>
+          <h5>Saturday, November 20, 2021</h5>
+          <Countdown date={Date.now() + 10000} renderer={renderer} />
+        </Details>
+      </DateInfo>
     </Container>
   );
 };
@@ -15,8 +38,9 @@ const Welcome = () => {
 export default Welcome;
 
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  grid-gap: 1rem;
   background: maroon;
 `;
 
@@ -33,13 +57,19 @@ const fadeOut = keyframes`
 const HeroImage = styled.div`
   display: inline-block;
   visibility: ${(props) => (props.out ? "hidden" : "visible")};
-  animation: ${(props) => (props.out ? fadeOut : fadeIn)} 5s linear;
-  transition: visibility 5s linear;
+  animation: ${(props) => (props.out ? fadeOut : fadeIn)} 3s linear;
+  transition: visibility 3s linear;
   width: 200px;
 `;
 
 const DateInfo = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  //   border: 1px solid yellow;
+`;
 
-  border: 1px solid yellow;
+const Details = styled.div`
+  color: white;
+  //   border: 1px dotted white;
 `;
