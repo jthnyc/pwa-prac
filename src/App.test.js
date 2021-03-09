11a3ca702ec7f05a6a03bc0de92d54ed7f98d899
Joanna Huang <joannathhuang@gmail.com>
2021-03-08 19:11:52 -0500

    adjust test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/November/i);
+  const linkElement = screen.getByText(/Saturday/i);
   expect(linkElement).toBeInTheDocument();
 });
