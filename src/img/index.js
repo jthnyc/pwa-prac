1d29d709be3bfb50c8e4e5c38348763383a360dd
Joanna Huang <joanna.huang@borngroup.com>
2021-04-24 09:09:46 -0400

    fixing missing img

--- src/img/index.js
+++ src/img/index.js
@@ -1,6 +1,6 @@
 export {default as g_bw} from "./g_bw.jpeg";
-export {default as j_color} from "./j_color.jpg";
-export {default as jg_bw} from "./J&G_0147.jpg";
-export {default as jg_rings} from "./jg_rings.jpg";
+export {default as j_color} from "./j_color.jpeg";
+export {default as jg_bw} from "./J&G_0147.jpeg";
+export {default as jg_rings} from "./jg_rings.JPG";
 export {default as heroImg} from "./heroImg.jpg";
-export {default as jg_color} from "./jngcolorview.jpg";
+export {default as jg_color} from "./jngcolorview.jpeg";
