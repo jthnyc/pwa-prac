cca03e7f4101d7a9fad5d0e95912640936d8cb4a
Joanna Huang <joanna.huang@borngroup.com>
2021-05-02 16:37:47 -0400

    modifying img file names

--- src/img/index.js
+++ src/img/index.js
@@ -2,6 +2,6 @@ export {default as g_bw} from "./g_bw.jpeg";
 export {default as j_color} from "./j_color.jpeg";
 export {default as jg_bw} from "./jg_bw.jpeg";
 export {default as jg_bw2} from "./jg_bw2.jpeg";
-export {default as jg_rings} from "./jg_rings.jpg";
-export {default as hero_img} from "./hero_img.jpg";
+export {default as jg_rings} from "./jg_rings.jpeg";
+export {default as hero_img} from "./hero_img.jpeg";
 export {default as jg_color} from "./jg_color.jpeg";
