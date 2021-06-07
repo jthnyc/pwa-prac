c2d9c404e4907f44728c92d29677ebc8b495e172
Joanna Huang <joannathhuang@gmail.com>
2021-06-07 18:56:33 -0400

    Update alt text for photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,22 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import {g_bw, j_color, jg_bw, jg_bw2, jg_color, jg_rings} from "../img/index";
 
 const Photos = () => {
   return (
-    <PhotoWrapper>
-      <img src={jg_bw} alt="jg_bw" />
-      <img src={jg_rings} alt="jg_rings" />
-      <img src={g_bw} alt="g_bw" />
-      <img src={j_color} alt="j_color" />
-      <img src={jg_bw2} alt="jg_bw2" />
-      <img src={jg_color} alt="jg_color" />
-    </PhotoWrapper>
+    <div>
+      <img src={jg_bw} alt="Joanna & Gabe glancing back near Bushwick Inlet Park" />
+      <img src={jg_rings} alt="Close up shot of wedding bands" />
+      <img src={g_bw} alt="Gabe looking afar on the balcony at William Vale" />
+      <img src={j_color} alt="Joanna on the balcony at William Vale" />
+      <img src={jg_bw2} alt="Joanna & Gabe on a side walk in Williamsburg" />
+      <img src={jg_color} alt="Joanna & Gabe on top of William Vale" />
+    </div>
   );
 };
 
 export default Photos;
-
-const PhotoWrapper = styled.div`
-  //   border: 10px solid green;
-`;
